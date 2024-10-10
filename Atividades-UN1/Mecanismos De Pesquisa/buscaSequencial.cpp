a125e86e2550c0cac5f066bf8880e7fd9e444dea
Alison Martins <alisonmarteso@gmail.com>
2024-10-09 22:58:02 -0300

    busca sequencial interativa

---
+++ Atividades-UN1/Mecanismos De Pesquisa/buscaSequencial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+int buscaSequencial(int arr[], int Xesperado){
+    int tam = sizeof(arr) / sizeof(arr[0]); 
+    for (int i = 0; i < tam; i++) {
+        if (arr[i] == Xesperado){
+            return i;
+        }}
+
+    return -1;    
+}
+
+
+int main () {}
